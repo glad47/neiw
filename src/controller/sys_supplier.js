c3c77719b8bc51af252fd5289b114fc3e4ec8213
ParkerT <1437477446@qq.com>
2018-10-08 20:01:41 +0800

    供应商信息添加，编辑存在bug未完成

--- src/controller/sys_supplier.js
+++ src/controller/sys_supplier.js
@@ -27,21 +27,21 @@ layui.define(['admin', 'table', 'index','element','form'], function(exports){
         }
         ,cols: [[
             {type:'checkbox'}
-            ,{field:'supplierId', title: '供应商编号', sort: true,width: '120'}
-            ,{field:'companyName', title: '公司名称',width: '180'} //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
-            ,{field:'type', title: '类别', sort: true,width: '120'}
-            ,{field:'contact', title: '联系人', sort: true,width: '120'}
-            ,{field:'officePhone', title: '办公电话', sort: true,width: '120'}
-            ,{field:'phone', title: '手机', align: 'center', sort: true,width: '120'} //单元格内容水平居中
-            ,{field:'email', title: 'Email', sort: true} //单元格内容水平居中
-            ,{field:'paymentType', title: '付款方式', sort: true}
-            ,{field:'taxe', title: '是否含税', sort: true}
-            ,{field:'invoiceType', title: '发票类型', sort: true}
-            ,{field:'strengths', title: '类型', sort: true}
-            ,{field:'evaluateDdelivery', title: '交期', sort: true}
-            ,{field:'evaluateQuality', title: '品质', sort: true}
-            ,{field:'evaluateCompatibility', title: '配合度', sort: true}
-            ,{field:'evaluateRate', title: '评级', sort: true}
+            ,{field:'supplierId', title: '供应商编号', sort: true,width: 110}
+            ,{field:'companyName', title: '公司名称',width: 180} //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+            ,{field:'type', title: '类别', sort: true,width: 120,templet: '#type'}
+            ,{field:'contact', title: '联系人', sort: true,width: 120}
+            ,{field:'officePhone', title: '办公电话', sort: true,width: 120}
+            ,{field:'phone', title: '手机', align: 'center', sort: true,width: 120} //单元格内容水平居中
+            ,{field:'email', title: 'Email', sort: true,width:200} //单元格内容水平居中
+            ,{field:'paymentType', title: '付款方式', sort: true,templet:'#paymentType'}
+            ,{field:'taxe', title: '是否含税', sort: true,templet:'#taxe'}
+            ,{field:'invoiceType', title: '发票类型', sort: true,templet:'#invoiceType'}
+            ,{field:'strengths', title: '强项类型', sort: true,templet: '#strengths',width: 110}
+            ,{field:'evaluateDdelivery', title: '交期', sort: true,templet: '#evaluateDdelivery'}
+            ,{field:'evaluateQuality', title: '品质', sort: true,templet: '#evaluateQuality'}
+            ,{field:'evaluateCompatibility', title: '配合度', sort: true,templet: '#evaluateCompatibility'}
+            ,{field:'evaluateRate', title: '评级', sort: true,templet: '#evaluateRate'}
             ,{field:'createUserId', title: '创建人', sort: true}
             ,{field:'createTime', title: '创建时间', sort: true}
             ,{field:'updateUserId', title: '修改人', sort: true}
@@ -57,7 +57,7 @@ layui.define(['admin', 'table', 'index','element','form'], function(exports){
         supplier_add:function(data){
             var this_id = $(this).attr('id');
             admin.popup({
-                title: '供应商信息编辑／添加'
+                title: '添加供应商信息'
                 ,shadeClose: true
                 ,shade: false
                 ,maxmin: true
@@ -66,18 +66,13 @@ layui.define(['admin', 'table', 'index','element','form'], function(exports){
                 ,success: function(layero, index){
                     view(this.id).render('/infoManagement/iframeWindow/supplier_edit', data).done(function(){
                         form.render(null, 'supplierAdd_form');
-                        // form.render(null, 'user_menuMe_form');
-
                         //监听提交
                         form.on('submit(LAY-supplier-add-submit)', function(data){
                             var field = data.field; //获取提交的字段
-                            console.log(field);
                             layer.alert(JSON.stringify(data.field));
                             admin.req({
                                 type:'post',
                                 url: setter.baseUrl+'sys/supplier/save' //实际使用请改成服务端真实接口
-                                // ,dataType: 'json'
-                                // ,contentType: 'application/json'
                                 ,data: field
                                 ,done: function(res){
                                     console.log(res);
@@ -87,17 +82,39 @@ layui.define(['admin', 'table', 'index','element','form'], function(exports){
                                     layer.msg('供应商加失败');
                                 },
                             });
-                            // layui.table.reload('sys_menu'); // 重载表格
-                            // layer.close(index); //执行关闭
                             layui.table.reload('supplier_infoTab'); //重载表格
                             layer.close(index); //执行关闭
                         });
                     });
                 }
             });
-
-
-
+        },
+        //供应商信息编辑
+        supplier_edit :function(data){
+            var checkStatus = table.checkStatus('supplier_infoTab')
+            ,data = checkStatus.data
+            ,twotest = JSON.stringify(data).substring(1, JSON.stringify(data).length -1)
+            var checkLen = data.length; //获取选中的行的数量
+            if (checkLen ===1){
+                admin.popup({
+                    title: '编辑供应商信息'
+                    ,shadeClose: true
+                    ,shade: false
+                    ,maxmin: true
+                    ,id: 'supplierAdd_form'
+                    ,area: ['55%', '65%']
+                    ,success: function (layero, index) {
+                        view(this.id).render('/infoManagement/iframeWindow/supplier_edit', data).done(function () {
+                            form.render(null, 'supplierAdd_form');
+                            console.log("data:"+data);
+                        })
+                    }
+                })
+            } else if (checkLen === 0 || checkLen == null || checkLen == ""){
+                layer.msg('请选择要编辑的行！！！');
+            } else if (checkLen > 1) {
+                layer.msg('只能选择一行，请重新选择！');
+            }
 
         },
         //供应商设置
