1a40f01d1296ca40a314ea62089f5446c16e7f70
ParkerT <1437477446@qq.com>
2018-12-24 14:06:12 +0800

    1

--- src/config.js
+++ src/config.js
@@ -44,6 +44,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       ,dataName: 'data' //数据详情的字段名称
     }
     ,baseUrl:'http://192.168.0.155:8080/renren-fast/'
+    ,baseUrl1:"http://192.168.0.155:8871/"
     ,webSocketUrl: 'ws://192.168.0.108:2200/ws'
     ,imUrl:'http://192.168.0.108:8899/lzimserver/'
 
