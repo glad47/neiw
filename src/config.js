2e15d023e8db3e11f13d4778dc2d3de5e6c6a71d
zl <heimazhoulei@163.com>
2018-12-24 19:41:50 +0800

    s

--- src/config.js
+++ src/config.js
@@ -43,10 +43,10 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
       ,msgName: 'msg' //状态信息的字段名称
       ,dataName: 'data' //数据详情的字段名称
     }
-    ,baseUrl:'http://192.168.0.155:8080/renren-fast/'
-    ,baseUrl1:"http://192.168.0.155:8871/"
-    ,webSocketUrl: 'ws://192.168.0.108:2200/ws'
-    ,imUrl:'http://192.168.0.108:8899/lzimserver/'
+    ,baseUrl:'http://www.pcbonline.com:8080/renren-fast/'
+    ,baseUrl1:"http://localhost:8871/"
+    ,webSocketUrl: 'ws://localhost:2200/ws'
+    ,imUrl:'http://localhost:8899/lzimserver/'
 
     //独立页面路由，可随意添加（无需写参数）
     ,indPage: [
