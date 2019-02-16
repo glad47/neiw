58aa658c38cf66967dcc6207f382ed5dcd48f210
ParkerT <1437477446@qq.com>
2019-02-16 20:12:50 +0800

    pcb订单字段顺序改变

--- src/controller/market_internalQuote.js
+++ src/controller/market_internalQuote.js
@@ -35,95 +35,97 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
         }
         ,cols: [[
             {type:'checkbox'}
-            ,{field: 'id', title: 'ID',hide:true}
-            ,{field: 'userId', title: 'User ID'}
-            ,{field: 'productNo', title: '客户编码',width: 130}
-            // ,{field:'status',fixed: 'left', title: '状态', hide: false, align:'center',templet: '#Tabtb-pcb-market-iQuote-status',width: 110}
-            ,{field:'status', title: '状态', hide: false, align:'center',templet: '#Tabtb-pcb-market-iQuote-status',width: 110}
-            ,{field: 'areaSq',title: '面积',edit: 'text'}
-            ,{field: 'bevellingCamfer',title: '斜边',edit: 'text'}
-            ,{field: 'bgaSize',title: 'BGA大小',edit: 'text'}
-            ,{field: 'blindHoles',title: '盲孔',edit: 'text'}
-            ,{field: 'boardFee',title: '板费',edit: 'text'}
-            ,{field: 'boardType',title: '板类型',edit: 'text'}
-            ,{field: 'buildTime',title: '交期',edit: 'text'}
-            ,{field: 'buriedHoles',title: '埋孔',edit: 'text'}
-            ,{field: 'carbon',title: '碳油',edit: 'text'}
-            ,{field: 'contrlImpeance',title: '阻抗',edit: 'text'}
-            ,{field: 'cti',title: 'CTI',edit: 'text'}
-            ,{field: 'deepMillRouting',title: '控深锣',edit: 'text'}
-            // ,{field: 'differentDesign',title: 'differentDesign',edit: 'text',hide: true}
-            ,{field: 'dimensionsX',title: '尺寸(X)',edit: 'text'}
-            ,{field: 'dimensionsY',title: '尺寸(Y)',edit: 'text'}
-            ,{field: 'edgePlated',title: '金属边',edit: 'text'}
-            ,{field: 'engineeringFee',title: '工程费',edit: 'text'}
+            ,{field: 'orderType',title: '订单类型',edit: 'text'}    //1=新单  2=返单    3=返单有改
+            ,{field: 'productNo', title: '内部编码',width: 130}
+            ,{field: 'gerberName',title: '文件名',edit: 'text',width: 160}
+            ,{field: 'pcbType',title: 'PCB类型',edit: 'text',width: 130}
+            // 型号占位
+            ,{field: 'dimensionsX',title: '单只(X)',edit: 'text'}
+            ,{field: 'dimensionsY',title: '单只(Y)',edit: 'text'}
+            ,{field: 'panelSizeX',title: 'Panel(X)',edit: 'text',width: 90}
+            ,{field: 'panelSizeY',title: 'Panel(Y)',edit: 'text',width: 90}
+            ,{field: 'panelWayX',title: 'PanelWay(X)',edit: 'text',width: 110}
+            ,{field: 'panelWayY',title: 'PanelWay(Y)',edit: 'text',width: 110}
             ,{field: 'finishThickness',title: '板厚',edit: 'text'}
-            ,{field: 'gerberName',title: '文件名',edit: 'text'}
-            ,{field: 'gerberPath',title: 'gerberPath',edit: 'text',hide: true}
-            ,{field: 'gmtCreate',title: '创建时间',edit: 'text'}
-            ,{field: 'gmtModified',title: '修改时间',edit: 'text'}
-            ,{field: 'halfHoleLated',title: '半孔板',edit: 'text'}
-            ,{field: 'halogenFree',title: '无卤素',edit: 'text'}
-            ,{field: 'heatConductivity',title: '导热系数',edit: 'text'}
-            ,{field: 'innerLayerCopper',title: '内层铜厚',edit: 'text'}
-            ,{field: 'innerMinSpacing',title: '内层(线宽/线距)',edit: 'text'}
-            // ,{field: 'innerMinTrack',title: 'innerMinTrack',edit: 'text',hide: true}
-            ,{field: 'invoiceNo',title: '合同号',edit: 'text'}
-            ,{field: 'isExistIndicator',title: 'isExistIndicator',edit: 'text'}
-            ,{field: 'isExistSmt',title: 'isExistSmt',edit: 'text'}
-            ,{field: 'layerNum',title: '层数',edit: 'text'}
-            ,{field: 'material',title: '材料',edit: 'text'}
-            ,{field: 'minHoleSize',title: '最小孔',edit: 'text'}
-            ,{field: 'nofCore',title: '芯板数量',edit: 'text'}
-            ,{field: 'nofHoles',title: '孔数',edit: 'text'}
-            ,{field: 'nofPp',title: 'PP数量',edit: 'text'}
-            ,{field: 'orderId',title: '订单ID',edit: 'text'}
-            ,{field: 'orderNo',title: '订单号',edit: 'text'}
-            ,{field: 'orderType',title: '订单类型',edit: 'text'}
-            ,{field: 'outerLayerCopper',title: '外层铜厚',edit: 'text'}
-            ,{field: 'outerMinSpacing',title: '外层最小(线宽/线距)',edit: 'text'}
-            // ,{field: 'outerMinTrack',title: 'outerMinTrack',edit: 'text',hide: true}
+            ,{field: 'layerNum',title: '层数',edit: 'text',width: 80}
+            ,{field: 'surfaceFinish',title: '表面处理',edit: 'text'}
+            ,{field: 'quantityPcs',title: 'PCS数量',edit: 'text'}
+            ,{field: 'quantityPanel',title: 'Panel数量',edit: 'text'}
+            ,{field: 'engineeringFee',title: '工程费',edit: 'text'}
+            ,{field: 'boardFee',title: '板费',edit: 'text'}
+            ,{field: 'testCostFee',title: '测试费',edit: 'text'}
+            ,{field: 'toolingFee',title: '工具费',edit: 'text'}
             ,{field: 'overworkFee',title: '加急费',edit: 'text'}
-            ,{field: 'panelRoutingPath',title: '锣程',edit: 'text'}
-            ,{field: 'panelSizeX',title: 'Panel(X)',edit: 'text'}
-            ,{field: 'panelSizeY',title: 'Panel(Y)',edit: 'text'}
-            ,{field: 'panelWayX',title: 'PanelWay(X)',edit: 'text'}
-            ,{field: 'panelWayY',title: 'PanelWay(Y)',edit: 'text'}
-            ,{field: 'pcbType',title: 'PCB类型',edit: 'text'}
-            ,{field: 'peelable',title: '兰胶',edit: 'text'}
-            ,{field: 'peelableBrand',title: '兰胶型号',edit: 'text'}
+            ,{field: 'postFee',title: '运费',edit: 'text'}
+            ,{field: 'subtotal',title: '总价',edit: 'text'}
+            ,{field: 'boardType',title: '出货方式',edit: 'text'}    // 1=单只 2=拼板
+            // ,{field:'status',fixed: 'left', title: '状态', hide: false, align:'center',templet: '#Tabtb-pcb-market-iQuote-status',width: 110}
+            ,{field:'status', title: '状态', hide: false, align:'center',templet: '#Tabtb-pcb-market-iQuote-status',width: 110,hide: true}
+            ,{field: 'areaSq',title: '面积',edit: 'text'}
+            ,{field: 'material',title: '材料',edit: 'text'}
             ,{field: 'productCode',title: '材料型号',edit: 'text'}
             ,{field: 'productNo',title: '材料商',edit: 'text'}
-            ,{field: 'pthCopper',title: '孔铜',edit: 'text'}
-            ,{field: 'punchingHoles',title: '冲孔数',edit: 'text'}
-            ,{field: 'punchingSlots',title: '冲槽数',edit: 'text'}
-            ,{field: 'quantityPanel',title: 'Panel数量',edit: 'text'}
-            ,{field: 'quantityPcs',title: 'PCS数量',edit: 'text'}
-            ,{field: 'quoteConfigIdList',title: 'quoteConfigIdList',edit: 'text',hide: true}
-            ,{field: 'quoteConfigIds',title: 'quoteConfigIds',edit: 'text',hide: true}
-            ,{field: 'quoteGerberName',title: 'quoteGerberName',edit: 'text'}
-            ,{field: 'quoteGerberPath',title: 'quoteGerberPath',edit: 'text'}
-            ,{field: 'remark',title: '备注',edit: 'text'}
-            ,{field: 'silkScreenBotColor',title: 'silkScreenBotColor',edit: 'text',hide: true}
+            ,{field: 'tg',title: 'TG',edit: 'text'}
+            ,{field: 'cti',title: 'CTI',edit: 'text'}
+            ,{field: 'halogenFree',title: '无卤素',edit: 'text'}
+            ,{field: 'outerLayerCopper',title: '外层铜厚',edit: 'text'}
+            ,{field: 'innerLayerCopper',title: '内层铜厚',edit: 'text'}
+            ,{field: 'heatConductivity',title: '导热系数',edit: 'text'}
             ,{field: 'silkScreenTopColor',title: '字符颜色',edit: 'text'}
-            ,{field: 'solderMaskBotColor',title: 'solderMaskBotColor',edit: 'text',hide: true}
-            ,{field: 'solderMaskTopColor',title: '阻焊',edit: 'text'}
+            ,{field: 'solderMaskTopColor',title: '阻焊颜色',edit: 'text'}
+            ,{field: 'pthCopper',title: '孔铜',edit: 'text'}
+            ,{field: 'remark',title: '备注',edit: 'text'}
+            ,{field: 'buildTime',title: '交期',edit: 'text'}
+            ,{field: 'weight',title: '重量',edit: 'text'}
+            ,{field: 'thickness',title: '表面处理厚度',edit: 'text'}
+            ,{field: 'surfaceArea',title: '沉金面积',edit: 'text'}
+            ,{field: 'gmtCreate',title: '创建时间',edit: 'text'}
+            ,{field: 'gmtModified',title: '修改时间',edit: 'text'}
+            // ,{field: 'innerMinTrack',title: 'innerMinTrack',edit: 'text',hide: true}
+            ,{field: 'minHoleSize',title: '最小孔',edit: 'text'}
+            ,{field: 'nofHoles',title: '孔数',edit: 'text'}
+            ,{field: 'viaProcess',title: '过孔方式',edit: 'text'}
             ,{field: 'stackUp',title: '压合',edit: 'text'}
             ,{field: 'status',title: '状态',edit: 'text'}
-            ,{field: 'subtotal',title: '总价',edit: 'text'}
-            ,{field: 'surfaceArea',title: '沉金面积',edit: 'text'}
-            ,{field: 'surfaceFinish',title: '表面处理',edit: 'text'}
-            ,{field: 'testCostFee',title: '测试费',edit: 'text'}
+            ,{field: 'nofCore',title: '芯板数量',edit: 'text'}
+            ,{field: 'nofPp',title: 'PP数量',edit: 'text'}
+            ,{field: 'innerMinSpacing',title: '内层(线宽/线距)',edit: 'text'}
+            ,{field: 'outerMinSpacing',title: '外层最小(线宽/线距)',edit: 'text'}
+            ,{field: 'bgaSize',title: 'BGA大小',edit: 'text'}
+            // ,{field: 'outerMinTrack',title: 'outerMinTrack',edit: 'text',hide: true}
             ,{field: 'testPoinType',title: '测试类型',edit: 'text'}
             ,{field: 'testPoint',title: '测试点',edit: 'text'}
             ,{field: 'testPointType',title: '测试方式',edit: 'text'}
-            ,{field: 'tg',title: 'TG',edit: 'text'}
-            ,{field: 'thickness',title: '表面处理厚度',edit: 'text'}
-            ,{field: 'toolingFee',title: '工具费',edit: 'text'}
             ,{field: 'userId',title: '用户ID',edit: 'text'}
             ,{field: 'uuid',title: 'uuid',edit: 'text'}
-            ,{field: 'viaProcess',title: '过孔方式',edit: 'text'}
-            ,{field: 'weight',title: 'weight',edit: 'text'}
+            ,{field: 'panelRoutingPath',title: '锣程',edit: 'text'}
+            ,{field: 'bevellingCamfer',title: '斜边',edit: 'text'}
+            ,{field: 'blindHoles',title: '盲孔',edit: 'text'}
+            ,{field: 'buriedHoles',title: '埋孔',edit: 'text'}
+            ,{field: 'carbon',title: '碳油',edit: 'text'}
+            ,{field: 'contrlImpeance',title: '阻抗',edit: 'text'}
+            ,{field: 'deepMillRouting',title: '控深锣',edit: 'text'}
+            ,{field: 'punchingHoles',title: '冲孔数',edit: 'text'}
+            ,{field: 'punchingSlots',title: '冲槽数',edit: 'text'}
+            ,{field: 'peelable',title: '兰胶',edit: 'text'}
+            ,{field: 'peelableBrand',title: '兰胶型号',edit: 'text'}
+            // ,{field: 'differentDesign',title: 'differentDesign',edit: 'text',hide: true}
+            ,{field: 'edgePlated',title: '金属边',edit: 'text'}
+            ,{field: 'halfHoleLated',title: '半孔板',edit: 'text'}
+            ,{field: 'orderId',title: '订单ID',edit: 'text'}
+            ,{field: 'orderNo',title: '订单号',edit: 'text'}
+            ,{field: 'invoiceNo',title: '合同号',edit: 'text'}
+            ,{field: 'id', title: 'ID',hide:true}
+            ,{field: 'userId', title: 'User ID',hide: true}
+            ,{field: 'gerberPath',title: 'gerberPath',edit: 'text',hide: true}
+            ,{field: 'isExistIndicator',title: 'isExistIndicator',edit: 'text',hide: true}
+            ,{field: 'isExistSmt',title: 'isExistSmt',edit: 'text',hide: true}
+            ,{field: 'quoteConfigIdList',title: 'quoteConfigIdList',edit: 'text',hide: true}
+            ,{field: 'quoteConfigIds',title: 'quoteConfigIds',edit: 'text',hide: true}
+            ,{field: 'quoteGerberName',title: 'quoteGerberName',edit: 'text',hide: true}
+            ,{field: 'quoteGerberPath',title: 'quoteGerberPath',edit: 'text',hide: true}
+            ,{field: 'silkScreenBotColor',title: 'silkScreenBotColor',edit: 'text',hide: true}
+            ,{field: 'solderMaskBotColor',title: 'solderMaskBotColor',edit: 'text',hide: true}
             ,{fixed: 'right', title:'操作', toolbar: '#iqpcbBar', width:70}
         ]]
         ,done: function (res, curr, count) {
@@ -164,16 +166,32 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
                             layer.alert("提交");
                         }
                         ,success: function (layero, index) {
-                            view(this.id).render(viewName, data).done(function () {
+                            view(this.id).render("marketManagement/iframeWindow/quote_contractA", data).done(function () {
                                 console.log(data);
                                 if (contractType === 1){
                                     // layui.each遍历的数据，td最少为6条，没有数据的显示空白
                                     var tdSize = $(".contract-module-three-tab tbody tr").eq(0).find("td").size();
                                     var dataLength = data.length;
-                                    for (var i=tdSize;i<7;i++){
+                                    var addTrNum;
+                                    if (dataLength <= 3){
+                                        addTrNum = 4;
+                                    } else if (dataLength > 3) {
+                                        addTrNum = 7;
+                                    }
+                                    for (var i=tdSize;i<addTrNum;i++){
+                                        console.log("增加td："+i);
                                         $(".contract-module-three-tab tbody").find("tr").append("<td></td>");
                                     }
-                                }
+                                    if (addTrNum == 4){
+                                        for (var i=1;i<addTrNum;i++){
+                                            $(".contract-module-three-tab tbody tr").find("td").eq(i).css({"width":"27.3%"});
+                                        }
+                                    } else {
+                                        for (var i=1;i<addTrNum;i++){
+                                            $(".contract-module-three-tab tbody tr").find("td").eq(i).css({"width":"13.6%"});
+                                        }
+                                    }
+                                    }
                                 // 实时时间设置
                                 let date = new Date();
                                 let chinaDate = date.toDateString();
