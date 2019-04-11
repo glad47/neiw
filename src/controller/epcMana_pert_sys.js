8f80e5d67d9fe6d68da32acaa62ebc6c0182d376
ParkerT <1437477446@qq.com>
2019-04-11 19:44:10 +0800

    1

--- src/controller/epcMana_pert_sys.js
+++ src/controller/epcMana_pert_sys.js
@@ -34,11 +34,11 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
 
     //－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－ PCB订单
     table.render({
-        elem: '#iqcMana_pertSys'
-        ,url: setter.baseUrl+'pert/revieworder/list?status=1'
-        ,toolbar: "#iqcMana_pertSys_tb"
+        elem: '#epcMana_pertSys'
+        ,url: setter.baseUrl+'pert/revieworder/list?status=2'
+        ,toolbar: "#epcMana_pertSys_tb"
         ,cellMinWidth: 80
-        ,id: "iqcMana_pertSys"
+        ,id: "epcMana_pertSys"
         ,page: true
         ,parseData: function (res) {
             return{
@@ -62,13 +62,13 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
             ,{field: 'quoteGerberName',title: '文件名', Width: 110, hide: true}
             ,{field: 'quoteGerberPath',title: '文件路径', Width: 110, hide: true}
             ,{field: 'id',title: 'ID', hide: true}
-            ,{fixed: 'right', title:'操作', toolbar: '#markerMana_tabbar',width: 150}
+            ,{fixed: 'right', title:'操作', toolbar: '#epcMana_pertSys_tabbar',width: 150}
         ]]
         ,done: function (res, curr, count) {
 
         }
     });
-    table.on('toolbar(iqcMana_pertSys)', function (obj) {
+    table.on('toolbar(eqcMana_pertSys)', function (obj) {
         var checkStatus = table.checkStatus(obj.config.id);
         var pertData = new Object();
         switch (obj.event) {
@@ -88,6 +88,7 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
                     }
                 });
                 break;
+
             case 'submit':
                 layer.msg(1);
                 var data = checkStatus.data;
@@ -97,37 +98,38 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
                     layer.msg("请选择一条要提交的数据！");
                 } else {
                     layer.confirm('确定提交评审？', function () {
-                       admin.req({
-                          type: 'post',
-                          url: setter.baseUrl + 'pert/revieworder/update',
-                          data: {id:data[0].id,status: 2},
-                          success: function () {
-                              layer.alert('评审提交成功！', function () {
-                                 table.reload('iqcMana_pertSys');
-                                 layer.closeAll();
-                              });
-                          } 
-                       });
+                        admin.req({
+                            type: 'post',
+                            url: setter.baseUrl + 'pert/revieworder/update',
+                            data: {id:data[0].id,status: 2},
+                            success: function () {
+                                layer.alert('评审提交成功！', function () {
+                                    table.reload('epcMana_pertSys');
+                                    layer.closeAll();
+                                });
+                            }
+                        });
                     });
                 }
                 break;
         }
     });
     //监听行工具事件＝＝＝＝》pcb订单
-    table.on('tool(iqcMana_pertSys)', function (obj) {
+    table.on('tool(eqcMana_pertSys)', function (obj) {
+        layer.msg(11);
         var data = obj.data;
         if (obj.event == 'edit'){
             data.opType = 'edit';
             admin.popup({
                 title: '修改评审订单'
-                ,area: ['506px','288px']
+                ,area: ['985px','501px']
                 ,btn: ['保存', '取消']
                 ,yes: function (index, layero) {
-                    $("#pertSysAddSubmit").click();
+
                 }
                 ,success: function (layero, index) {
-                    view(this.id).render('marketManagement/iframeWindow/pert_sysAdd',data).done(function () {
-
+                    view(this.id).render('epcManagement/iframeWindow/pert_pcbDetail',data).done(function () {
+                        form.render();
                     });
                 }
             });
@@ -144,7 +146,40 @@ layui.define(['admin','table','index','element','form','laydate'], function (exp
                     }
                 });
             });
+        } else if (obj.event == 'detail') {
+            admin.popup({
+                title: '编辑PCB订单信息'
+                ,btn:['立即提交', '取消']
+                ,area: ['76%', '90%']
+                ,yes: function () {
+                    $(".submit-ok").click();
+                }
+                ,success: function (layero, index) {
+                    view(this.id).render('marketManagement/iframeWindow/orderPCB_update', data).done(function () {
+                        console.log(data);
+                        form.render(null, '');
+                        form.on('submit(LAY-pcborder-update-submit)',function (data) {
+                            var field = data.field;
+                            console.log("提交的字段信息："+JSON.stringify(field));
+                            admin.req({
+                                type: 'post'
+                                ,url: setter.baseUrl+'pert/pcbinfo/save'
+                                ,data: field
+                                ,done: function (res) {
+                                    layer.msg('订单信息修改成功');
+                                    layui.table.reload('interior_order_Tabpcb');
+                                }
+                                ,fail: function (res) {
+                                    layer.msg("订单信息修改失败，请稍后再试！");
+                                },
+                            });
+                            layer.close(index);
+                            return false;
+                        });
+                    });
+                }
+            });
         }
     });
-    exports('iqcMana_pert_sys', {});
+    exports('epcMana_pert_sys', {});
 });
