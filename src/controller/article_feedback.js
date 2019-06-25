ef3882ffa15bb52c738a0cb3f1c59880b6997df8
ParkerT <1437477446@qq.com>
2019-06-25 16:51:51 +0800

    反馈添加

--- src/controller/article_feedback.js
+++ src/controller/article_feedback.js
@@ -27,7 +27,7 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit'], f
     table.render({
         elem: '#article_Table_feedback'
         ,url: setter.baseUrl+'/article/list'
-        ,toolbar: true
+        ,toolbar: '#toolbarFeedback'
         ,cellMinWidth: 80
         ,id:"article_Table_feedback"
         ,page: true
@@ -43,18 +43,17 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit'], f
         }
         ,cols: [[
             {field:'id', title: 'ID',hide: true}
-            ,{field:'articleName', title: '文章名称', hide: false, align:'center',width: 110}
-            ,{field:'articleTime', title: '发布时间', align:'center', width: 165}
-            ,{field:'articleIp', title: '发布ip', align:'center', width: 165}
-            ,{field:'articleClick', title: '查看人数', align:'center', width: 114}
-            ,{field:'articleLike', title: '点赞数', align:'center', width: 114}
-            ,{field:'articleComment', title: '评论数', align:'center', width: 114}
-            ,{field:'articleClassify', title: '文章分类', align:'center', width: 134}
-            ,{field:'articleType', title: '文章类型', align:'center', width: 114,templet:'#Tabtb-feedback-type'}
-            ,{field:'articleUserName', title: '文章发布用户', align:'center', width: 130}
-            ,{field:'articleStatus', title: '文章状态', align:'center', width: 114 ,templet: '#Tabtb-feedback-management-status'}
-
-            ,{title: '操作', width: 360, align:'center', fixed: 'right', toolbar: '#Tabtb-feedback-management-option'}
+            ,{field:'articleName', title: '文章名称', hide: false, align:'center'}
+            ,{field:'articleTime', title: '发布时间', align:'center', width: 176}
+            ,{field:'articleIp', title: '发布ip', align:'center'}
+            ,{field:'articleClick', title: '查看人数', align:'center'}
+            ,{field:'articleLike', title: '点赞数', align:'center'}
+            ,{field:'articleComment', title: '评论数', align:'center'}
+            ,{field:'articleClassify', title: '文章分类', align:'center'}
+            ,{field:'articleType', title: '文章类型', align:'center',templet:'#Tabtb-feedback-type'}
+            ,{field:'articleUserName', title: '文章发布用户', align:'center'}
+            ,{field:'articleStatus', title: '文章状态', align:'center',templet: '#Tabtb-feedback-management-status'}
+            ,{title: '操作', width: 120, align:'center', fixed: 'right', toolbar: '#Tabtb-feedback-management-option'}
         ]]
         ,done : function (res, curr, count) {
             //手机端
@@ -75,7 +74,7 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit'], f
         console.log(data);
         if (obj.event === 'edit') {
             admin.popup({
-                title: '修改Blog',
+                title: '修改反馈',
                 area: ['750px', '750px'],
                 btn: ['提交', '取消'],
                 yes: function (index, layero) {
@@ -213,7 +212,7 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit'], f
                                 url:setter.imUrl+'file/fileupload'
                             }
                         });
-                        //监听提交
+                        // 监听提交
                         form.on('submit(layuiadmin-app-form-submit)', function (data) {
                             var field = data.field;
                             var c = layedit.getContent(edit);
@@ -250,59 +249,54 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit'], f
         }
     });
 
-    var  active = {
-        blog_add: function (othis) {
-            clickTr = {};
-            admin.popup({
-                title: '添加Blog',
-                area: ['750px', '750px'],
-                btn: ['提交', '取消'],
-                yes: function (index, layero) {
-                    $('#layuiadmin-app-form-submit').click();
-                },
-                btn2: function (index, layero) {
-                },
-                end: function () {
-                    clickTr = {};
-                },
-                id: 'LAY-popup-role-add',
-                success: function (layero, index) {
-                    view(this.id).render('articleManagement/blogformadd').done(function () {
-                        //清空form表单
-                        form.render(null,'layuiadmin-app-form-list');
-                        var i = layedit.build('blogcontent',{
-                            uploadImage:{
-                                url:''
-                            }
-                        });
-                        //监听提交
-                        form.on('submit(layuiadmin-app-form-submit)', function (data) {
-                            var field = data.field;
-                            console.log(field);
-                            var c = layedit.getContent(i);
-                            console.log(c);
-                            field.articleContent = c;
-                            admin.req({
-                                url: setter.baseUrl + 'article/save',
-                                type: 'POST',
-                                //dataType:'json',
-                                //contentType:'application/json',
-                                data: field,
-                                success: function (data) {
-                                    console.log(data);
-                                    layui.table.reload('article_Table_feedback'); //重载表格
-                                    layer.close(index); //执行关闭
-                                }
-                            })
+    table.on('toolbar(article_Table_feedback)', function(obj) {
+       switch (obj.event) {
+           case 'add':
+               admin.popup({
+                   title: '添加反馈',
+                   area: ['750px', '750px'],
+                   btn: ['提交', '取消'],
+                   yes: function () {
+                       $('#layuiadmin-app-form-submit').click();
+                   },
+                   btn2: function (index, layero) {
+                   },
+                   end: function () {
+                       clickTr = {};
+                   },
+                   id: 'feedbackAdd-popu',
+                   success: function (layero, index) {
+                       view(this.id).render('articleManagement/feedbackfrom').done(function () {
+                           //清空form表单
+                           form.render(null,'layuiadmin-app-form-list');
+                           var edit = layedit.build('blogcontent',{
+                               uploadImage:{
+                                   url:setter.imUrl+'file/fileupload'
+                               }
+                           });
+                           // 监听提交
+                           form.on('submit(layuiadmin-app-form-submit)', function (data) {
+                               var field = data.field;
+                               var c = layedit.getContent(edit);
+                               field.articleContent = c;
+                               admin.req({
+                                   url: setter.baseUrl + 'article/saveFeedback',
+                                   type: 'POST',
+                                   data: field,
+                                   success: function (data) {
+                                       console.log(data);
+                                       layui.table.reload('article_Table_feedback'); //重载表格
+                                       layer.close(index); //执行关闭
+                                   }
+                               })
 
-                        });
-                    });
-                }
-            });
-        }
-    }
-
-
+                           });
+                       });
+                   }
+               })
+               break;
+       }
+    });
 
     $('.test-table-operate-btn .layui-btn').on('click', function () {
             var type = $(this).data('type');
