b8f4046ce53b8843371e13497860c55978e44570
zl <heimazhoulei@163.com>
2020-06-11 17:16:18 +0800

    fff

--- src/controller/order_pd_List.js
+++ src/controller/order_pd_List.js
@@ -113,15 +113,12 @@ layui.define(['admin','table','index','element','form', 'laydate'], function (ex
            } else {
                rowData.popMonthlyStatisticalType = getMSType();
                rowData.popMSTime = getMSTime();
-               var gr =  rowData.filter(function(item){
-                   return item.customerUserPaVO.isSourceCompany === 1;
-               });
-               var gs = rowData.filter(function(item){
-                   return item.customerUserPaVO.isSourceCompany === 0;
-               });
+               var gr =  rowData.filter(item => item.customerUserPaVO.isSourceCompany == 0);
+               var gs = rowData.filter(item => item.customerUserPaVO.isSourceCompany == 1);
+               console.log('gr',gr);
+               console.log('gs',gs);
                rowData.gr = gr;
                rowData.gs = gs;
-               console.log('ffff',rowData);
                admin.popup({
                    title: '月度统计表'
                    ,area: ['100%', '100%']
