4083bfb75e635c198694cfc2ed9e98f4f89df3cb
zl <heimazhoulei@163.com>
2020-08-03 11:38:30 +0800

    完善

--- src/lib/extend/tableSearch.js
+++ src/lib/extend/tableSearch.js
@@ -10,10 +10,9 @@ layui.define(['admin', 'index', 'form'],function (exports) {
         admin = layui.admin;
 
     var $form;
-    var formSelectsArr = [];    // 监听 select 对象数组
+    // var formSelectsArr = [];    // 监听 select 对象数组
     var formSubmitFilter;       // 表单搜索按钮 lay-filter
     var reloadTab;              // 当前要重载的表格
-    var lock = false;
     $(document).on('click', function (e) {
         // formSelectsArr.splice(0, formSelectsArr.length);    // 清空数组
         $form = $(e.target).closest(".layui-form"); // 点击的Form
@@ -21,6 +20,10 @@ layui.define(['admin', 'index', 'form'],function (exports) {
         if (_d_type == 'search-form') {
             reloadTab = $form.attr("reload-table");
             formSubmitFilter = $form.find(".layui-btn[lay-submit]").attr("lay-filter");
+            // $form.find("select").each(function (obj) {  // 获取所有下拉，赋值到数组
+            //     formSelectsArr.push($(this).attr('lay-filter'));    // 获取要监听 select 的 lay-filter
+            // });
+            // obj.formOnSelect();
             obj.formOnSubmit();
         }
     });
@@ -28,15 +31,14 @@ layui.define(['admin', 'index', 'form'],function (exports) {
         if(e.which == 13){
             reloadTab = $form.attr("reload-table"); 
             formSubmitFilter = $form.find(".layui-btn[lay-submit]").attr("lay-filter");
-            //obj.formOnSubmit();
             $("button[lay-filter='"+ formSubmitFilter + "']").click();
         }
     });
 
     var obj = {
         formOnSelect: function (reTab) {     // 监听  select
-            var $this = this;
             if (formSelectsArr.length > 0) {
+                console.log('监听select的表单提交')
                 for (var i=0;i<formSelectsArr.length; i++) {    // 下拉监听
                     form.on('select('+ formSelectsArr[i] +')', function (data) {
                         $("button[lay-filter='"+ formSubmitFilter + "']").click();
@@ -46,8 +48,9 @@ layui.define(['admin', 'index', 'form'],function (exports) {
         },
         formOnSubmit: function () {     // 监听 表单提交
           form.on('submit('+ formSubmitFilter +')', function (data) {
+            // console.log('表单提交');
              var field = data.field;
-            //   obj.tabReload(reloadTab, field);
+              obj.tabReload(reloadTab, field);
               table.reload(reloadTab,{
                   where: field
               })
