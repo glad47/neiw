0801c71ba8a5afd3f064dafc7721cd374a8cb423
zl <heimazhoulei@163.com>
2020-10-19 17:26:47 +0800

    fffff

--- src/controller/article_feedback.js
+++ src/controller/article_feedback.js
@@ -211,7 +211,7 @@ layui.define(['admin', 'table', 'index','element','form','laydate','layedit', 'f
                         });
                         var edit = layedit.build('blogcontent',{
                             uploadImage:{
-                                url:setter.imUrl+'file/fileupload'
+                                url:setter.imUrl+'imgfile/fileupload'
                             }
                         });
                         // 监听提交
