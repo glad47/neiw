3fdd6f8a66ad5bfde05e0eb417858bb4af4a04fb
zl <heimazhoulei@163.com>
2021-01-07 10:05:18 +0800

    f

--- src/controller/epc_indicatorCard.js
+++ src/controller/epc_indicatorCard.js
@@ -161,7 +161,7 @@ layui.define(['admin', 'table', 'index','element','form','laydate','upload', 'up
                 ,{field:'nofCore', title: 'NofCore', align:'center', width: 80,hide: true}
                 ,{field:'nofPp', title: 'NofPp', align:'center', width: 80,hide: true}
                 ,{field:'nofHoles', title: 'NofHoles', align:'center', width: 90,hide: true}
-                ,{title: '操作', width: 320, align:'center', fixed: 'right', toolbar: '#Tabtb-pcb-epc-indicatorCard-option'}
+                ,{title: '操作', width: 380, align:'center', fixed: 'right', toolbar: '#Tabtb-pcb-epc-indicatorCard-option'}
             ]]
             ,done : function (res, curr, count) {
                 $("a[data='isOk']").each(function (i, n) {
